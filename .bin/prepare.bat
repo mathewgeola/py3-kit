73e6fe42680c2222fb040ea096a8c680aa013abf
mathewgeola <mathewgeola@gmail.com>
2026-01-07 15:30:36 +0800

    version = "v0.0.1"

---
+++ .bin/prepare.bat
@@ -0,0 +1,17 @@
+@echo off
+
+call ..\.venv\Scripts\activate.bat
+
+set /p VERSION=<../VERSION
+
+for /f "tokens=* delims= " %%a in ("%VERSION%") do set VERSION=%%a
+
+poetry version %VERSION%
+
+for /f %%i in ('git merge-base HEAD origin/main') do set BASE=%%i
+
+git reset %BASE%
+
+git add -A
+
+git commit -m "version = \"v%VERSION%\""
---
+++ .bin/prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ../.venv/bin/activate
+
+read -r VERSION < ../VERSION
+
+VERSION=$(echo "$VERSION" | xargs)
+
+poetry version "$VERSION"
+
+BASE=$(git merge-base HEAD origin/main)
+
+git reset "$BASE"
+
+git add -A
+
+git commit -m "version = \"v$VERSION\""
